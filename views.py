8067c0063f182007b3caa1fbfe29f88497d1ec65
Ayush gupta <39850559+Ayushmn@users.noreply.github.com>
2021-05-27 21:55:57 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
-from .models import Destination
+from django.contrib.auth.models import user,auth
 # Create your views here.
 
 
-def index(request):
-
-
-
-    dests = Destination.objects.all()
-
-    return render(request, "index.html", {'dests': dests})
+def register(request):
